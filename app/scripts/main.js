635fa0bf18696cda02adfa5d0e250bdb80a8802d
Neil Lohana <Neil_lohana@hotmail.com>
2016-03-01 22:18:21 -0500

    Setup random quote api and ajax request

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(document).ready(function(){
+    console.log('Loaded main.js!');
+
+    $('#generate').on('click', function(){
+        // Function to fetch the quote when button clicked
+        $.ajax({
+            type: 'POST',
+            url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
+            headers: {
+                "X-Mashape-Key": "P7SzoGf5HWmshu3NYRC3l627SRJEp1NddF6jsnvGYBqiCEVQ9l",
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Accept': 'application/json'
+            }
+        }).done(function(){
+            $('#quote').text('Quote changed!');
+        });
+    });
+});
